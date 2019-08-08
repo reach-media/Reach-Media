86d8c9c7173096d2dac1c1078ac2dd2d02eb8edd
Gilbert Baracka <gilbert.baracka@gmail.com>
2019-08-07 23:57:19 -0600

    header out

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
-import "./App.css";
 
-//imported files
-import Header from "./Header/Header";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      {/* <div id="testing">
-        <div
-          colors={["#00e4d0", "#5983e8"]}
-          style={{ borderBottomLeftRadius: 100, borderBottomRightRadius: 100 }}
-        />
-      </div> */}
+    <div>
+
+
     </div>
   );
 }

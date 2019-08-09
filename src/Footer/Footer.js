dac8a7f2029f8cf0a3d47953738050bc8f362b63
Gilbert Baracka <gilbert.baracka@gmail.com>
2019-08-08 19:57:15 -0600

    file

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.scss'
+
+const Footer = ()=> {
+    return (
+        <footer>
+            <section>
+                <h3>TALK TO US</h3>
+                <p>801-123-1234</p>
+            </section>
+        </footer>
+    )
+}

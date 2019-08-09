ba13b3ea9f7d379ddcd5250d8d9fe9ea44c23ac1
Hackerrr23 <yusefdaramay@yahoo.com>
2019-08-09 11:39:06 -0500

    first page done, not responsive yet

--- src/ChooseUs/ChooseUs.js
+++ src/ChooseUs/ChooseUs.js
@@ -5,7 +5,7 @@ import "./ChooseUs.css";
 import B2B from "../images/B2B-View-Case-Studies.png";
 export default function ChooseUs() {
   return (
-    <div>
+    <div id="partner">
       <div id="facts">
         <div className="first_andthird_statements">
           <div>
@@ -33,19 +33,30 @@ export default function ChooseUs() {
           </div>
         </div>
       </div>
-      <div id="why_partner_withUs">
-        <div id="wh_us">
-          <h1>Why Partner with Us</h1>
+      <div className="why_partner_withUs">
+        <div className="why_us">
+          <h1>Why Partner With Us</h1>
           <p>
-            With us you will get an experienced team that to build and maintain
-            your presense online.
+            With us, you will get an experienced team dedicated to building and
+            maintainingy your presense online.
           </p>
-          <br />
-          <br />
+          {/* <br /> */}
           <p>Our goal is for you to run your business as easily as possible.</p>
         </div>
         <div id="why_us_pic" />
       </div>
+      <div className="why_partner_withUs">
+        <div id="why_us_pic2" />
+        <div className="why_us">
+          <h1>Meet With Us</h1>
+          <p>
+            During Our Meeting, we will learn all about your business needs,
+            customer tendencies, and overall vision to tailor a strategy just
+            for your business or website.
+          </p>
+        </div>
+      </div>
+      <h1 id="connect">LET'S CONNECT!</h1>
     </div>
   );
 }

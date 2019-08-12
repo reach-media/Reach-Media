b38cc9db7e12b48308c47f50f70e6245b5f08b88
Fethi E <jack@jacks-MBP-2.satx.rr.com>
2019-08-12 18:53:47 -0500

    fethi initial commit

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import Main from "./Main/Main";
+import Contact from "./ContactUs/ContactUs"
 
 export default (
   <Switch>
     <Route exact path="/" component={Main} />
+    <Route path = "/contact" component = {Contact}/>
   </Switch>
 );

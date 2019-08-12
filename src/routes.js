915b4fcd15dd3937d554756c916b25c597994da0
Gilbert Baracka <gilbert.baracka@gmail.com>
2019-08-11 23:57:27 -0600

    last for night

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import Ourwork from './Our-work/OurWork'
 import Blog from "./Blog/Blog"
-
 import Main from "./Main/Main";
 
 export default (

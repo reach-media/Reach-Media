c1bcbcd1a154def745eee563d2798020402c5907
Hackerrr23 <yusefdaramay@yahoo.com>
2019-08-15 21:36:13 -0500

    more work on the what we do page

--- src/WhatWeDo/WhatWeDo.js
+++ src/WhatWeDo/WhatWeDo.js
@@ -1,11 +1,68 @@
 import React from "react";
 import "./WhatWeDo.css";
+import { Link } from "react-router-dom";
+
+import Pic from "../assets/343b0ec396800d1cbd0c0e6939e5afca_XL-compressor.jpg";
 export default function WhatWeDo() {
   return (
-    <div id="what">
-      <div id="what_we_do">
-        <h1>You Have Bright Ideas. We Bring Them To Life</h1>
-        <h2>A One-Stop Solution for All Your Online Needs</h2>
+    <div>
+      <div id="what">
+        <div id="what_we_do">
+          <h1>You Have Bright Ideas. We Bring Them To Life</h1>
+          <h2>A One-Stop Solution for All Your Online Needs</h2>
+        </div>
+      </div>
+      <h1>How We Work</h1>
+      <div className="technique">
+        <div className="word_side">
+          <h1 className="technique_description">Discovery And Research</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            cupiditate veritatis, facilis incidunt, debitis minus aperiam, nulla
+            pariatur ea praesentium consequatur deleniti sed numquam? Nihil
+            iusto possimus a consectetur sunt!
+          </p>
+          <Link to="/contactus">
+            <button>Work With Us</button>
+          </Link>
+        </div>
+        <div className="picture_side">
+          <img src={Pic} alt="" />
+        </div>
+      </div>
+      <div className="technique">
+        <div className="word_side">
+          <h1 className="technique_description">hello</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            cupiditate veritatis, facilis incidunt, debitis minus aperiam, nulla
+            pariatur ea praesentium consequatur deleniti sed numquam? Nihil
+            iusto possimus a consectetur sunt!
+          </p>
+          <Link to="/contactus">
+            <button>Work With Us</button>
+          </Link>
+        </div>
+        <div className="picture_side">
+          <img src={Pic} alt="" />
+        </div>
+      </div>
+      <div className="technique">
+        <div className="word_side">
+          <h1 className="technique_description">DisCover And Research</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+            cupiditate veritatis, facilis incidunt, debitis minus aperiam, nulla
+            pariatur ea praesentium consequatur deleniti sed numquam? Nihil
+            iusto possimus a consectetur sunt!
+          </p>
+          <Link to="/contactus">
+            <button>Work With Us</button>
+          </Link>
+        </div>
+        <div className="picture_side">
+          <img src={Pic} alt="" />
+        </div>
       </div>
     </div>
   );

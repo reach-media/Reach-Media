16fccbc4cc6f2feab0b88438f4ab06edb7b452f4
Yusfu Daramay <yousufdaramay@yahoo.com>
2019-09-08 20:05:35 -0500

    fixed form again

--- src/ContactUs/ContactUs.js
+++ src/ContactUs/ContactUs.js
@@ -2,7 +2,7 @@ import React, { Component } from "react"
 import axios from "axios"
 import "./ContactUs.scss"
 import Fab from "@material-ui/core/Fab";
-
+import Email from "../functions/email"
 const encode = (data) => {
     return Object.keys(data)
         .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
@@ -21,60 +21,48 @@ class Contact extends Component {
         }
     }
     handleSubmit = e => {
-        fetch("/", {
-          method: "POST",
-          headers: { "Content-Type": "application/x-www-form-urlencoded" },
-          body: encode({ "form-name": "contact", ...this.state })
+        const { name, email, phone, company, interestedIn, message } = this.state
+        e.preventDefault()
+        console.log(Email.handler())
+        // axios
+        //     .post('/api/contact', { name, email, phone, company, interestedIn, message })
+        //     .then(() => this.setState({
+        //         isMailSent: true,
+        //         name: "",
+        //         email: "",
+        //         phone: "",
+        //         company: "",
+        //         interestedIn: "",
+        //         message: "",
+        //         checkbox: false,
+        //         error: false,
+        //     }))
+        //     .catch(() => this.setState({
+        //         error: true,
+        //         name: "",
+        //         email: "",
+        //         phone: "",
+        //         company: "",
+        //         interestedIn: "",
+        //         message: "",
+        //         checkbox: false,
+        //         isMailSent: false,
+        //     }))
+    }
+    handleChange = e => {
+        e.preventDefault()
+        console.log(e.target.value)
+        this.setState({
+            [e.target.name]: e.target.value
         })
-          .then(() => alert("Success!"))
-          .catch(error => alert(error));
+    }
+
+    handleChecked = e => {
+        this.setState({ checkbox: !this.state.checkbox })
+
+    }
   
-        e.preventDefault();
-      };
-  
-      handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-    // handleChange = e => {
-    //     e.preventDefault()
-    //     console.log(e.target.value)
-    //     this.setState({
-    //         [e.target.name]: e.target.value
-    //     })
-    // }
-
-    // handleChecked = e => {
-    //     this.setState({ checkbox: !this.state.checkbox })
-
-    // }
-
-    // handleSubmit = e => {
-    //     const { name, email, phone, company, interestedIn, message } = this.state
-    //     e.preventDefault()
-    //     axios
-    //         .post('/api/contact', { name, email, phone, company, interestedIn, message })
-    //         .then(() => this.setState({
-    //             isMailSent: true,
-    //             name: "",
-    //             email: "",
-    //             phone: "",
-    //             company: "",
-    //             interestedIn: "",
-    //             message: "",
-    //             checkbox: false,
-    //             error: false,
-    //         }))
-    //         .catch(() => this.setState({
-    //             error: true,
-    //             name: "",
-    //             email: "",
-    //             phone: "",
-    //             company: "",
-    //             interestedIn: "",
-    //             message: "",
-    //             checkbox: false,
-    //             isMailSent: false,
-    //         }))
-    // }
+    //   handleChange = e => this.setState({ [e.target.name]: e.target.value });
 
     render() {
         const { name, email, phone, company,  message } = this.state
@@ -88,13 +76,6 @@ class Contact extends Component {
                     <h3>
                         Get started by filling out the form below. We will be sure to contact you!
                     </h3>
-                    {/* <h3><strong>- or -</strong></h3>
-                    <div id="myfab">
-
-                    <Fab variant="extended" aria-label="delete" color='secondary' >
-                        BOOK A FREE CONSULTATION
-                    </Fab>
-                    </div> */}
                 </div>
                 <div className="container" id="third-section">
                     <div id="left-column">
@@ -105,33 +86,8 @@ class Contact extends Component {
                         <p><em>- Are you just getting started?</em></p>
                         <p><em>- Is Social Media right for your business?</em></p>
                         <p><em>- Are you getting as many sales as you would like?</em></p>
-                        
-                        {/* <h5 style={{ color: "red" }}>Get in touch!</h5>
-                        <p><strong>Email:</strong>reachmedia@gmail.com</p>
-                        <p><strong>Phone:</strong>832-952-7770</p> */}
-                        {/* <p><strong>Address:</strong>The cupboard under the stairs,4 privet drive, little whinging, surrey</p> */}
                     </div>
-                    <form name="contact" method="POST" data-netlify="true" onSubmit={this.handleSubmit}>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-                    {/* <form id="right-column" name="contact" method="POST" data-netlify="true" onSubmit={this.handleSubmit}>
+                    <form id="right-column" name="contact" method="POST" data-netlify="true" onSubmit={this.handleSubmit}>
                         <div class="row inputs">
                             <div class="col">
                                 <input type="text" class="form-control contact_inputs" placeholder="Name" name="name" value={name} onChange={this.handleChange} />
@@ -148,42 +104,24 @@ class Contact extends Component {
                             <div class="col">
                                 <input type="text" class="form-control contact_inputs" placeholder="Company" name="company" value={company} onChange={this.handleChange} />
                             </div>
-                        </div> */}
+                        </div>
                         <br />
-                        {/* <div class="form-group">
-                            <select id="inputState" class="form-control" name="interestedIn" value={interestedIn} onChange={this.handleChange}>
-                                <option selected>Interested In</option>
-                                <option>Web Design</option>
-                                <option>Content Writing</option>
-                                <option>Branding Strategy</option>
-                                <option>Graphic Design</option>
-                                <option>Social Media Management</option>
-                                <option>Marketing Mentor</option>
-                                <option>Other</option>
-                            </select>
-                        </div> */}
-                        {/* <div class="form-group">
+                        <div class="form-group">
                             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="message" value={message} onChange={this.handleChange}></textarea>
                         </div>
-                        <button type="submit">Send</button> */}
-                        {/* <div class="form-group">
+                        <div class="form-group">
                             <div class="form-check">
                                 <input class="form-check-input" type="checkbox" id="gridCheck" onChange={this.handleChecked} required />
                                 <label class="form-check-label" for="gridCheck">
                                     By submitting this form you agree with the storage and handling of your data by this website.
                                 </label>
                             </div>
-                        </div> */}
-                        {/* {isMailSent ? <div className="alert alert-success" role="alert">Email sent successfully</div>
-                            : null}
-                        {error ? <div className="alert alert-danger" role="alert">Oppps! Something went wrong</div>
-                            : null} */}
-
-                        {/* <button type="submit" class="btn btn-danger">Submit</button> */}
+                        </div>
+                        <button type="submit" class="btn btn-danger">Submit</button>
                         {/* name = "checkbox" value ={checkbox} onClick = {this.handleClick} */}
-                    {/* </form> */}
+                    </form>
                 </div>
-            </div>
+                </div>
         )
     }
 }
